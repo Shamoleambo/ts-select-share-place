8878d90b17d4ec301f90b3ea552154890b53a112
Tiago Gomes <tidgomes@gmail.com>
2023-05-17 09:13:45 -0300

    retrieve form and input from index.html

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+function searchAddressFunction(event: Event) {
+  event.preventDefault()
+  
+  const addedAddress = addressInput.value
+}
+
+form.addEventListener("submit", searchAddressFunction);
