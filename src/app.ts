6255e417408795ecac9cc0e85fb3270c6e4cfdda
Tiago Gomes <tidgomes@gmail.com>
2023-05-17 10:36:33 -0300

    configure axios to make an http request to google geocoding api

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import axios from 'axios'
 
-const form = document.querySelector("form")!
+const form = document.querySelector('form')!
 const addressInput = document.getElementById('address')! as HTMLInputElement
 const GOOGLE_API_KEY = `AIzaSyBd09LIdN-j_0zkhJQyOan6Lrowe0RSHJs`
 
 function searchAddressFunction(event: Event) {
   event.preventDefault()
 
-  const addedAddress = addressInput.value
+  const enteredAddress = addressInput.value
 
-  
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 
-form.addEventListener("submit", searchAddressFunction);
+form.addEventListener('submit', searchAddressFunction)
