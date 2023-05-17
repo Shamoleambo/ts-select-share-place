d9699d53c12a1162bc7d036d8074eda94edb1407
Tiago Gomes <tidgomes@gmail.com>
2023-05-17 10:28:42 -0300

    install axios dependency

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-const form = document.querySelector("form")!;
+import axios from 'axios'
+
+const form = document.querySelector("form")!
 const addressInput = document.getElementById('address')! as HTMLInputElement
+const GOOGLE_API_KEY = `AIzaSyBd09LIdN-j_0zkhJQyOan6Lrowe0RSHJs`
 
 function searchAddressFunction(event: Event) {
   event.preventDefault()
-  
+
   const addedAddress = addressInput.value
+
+  
 }
 
 form.addEventListener("submit", searchAddressFunction);
