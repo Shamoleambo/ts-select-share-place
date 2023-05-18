42ff87b128459ba7e39e37efc95fcf5b3b1be58c
Tiago Gomes <tidgomes@gmail.com>
2023-05-18 09:57:09 -0300

    omit certain details

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const form = document.querySelector('form')!
 const addressInput = document.getElementById('address')! as HTMLInputElement
-const GOOGLE_API_KEY = `AIzaSyBd09LIdN-j_0zkhJQyOan6Lrowe0RSHJs`
+const GOOGLE_API_KEY = ``
 
 declare var google: any
 
